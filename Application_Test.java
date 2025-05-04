f3540d6bfa2723f8fa8399df045903b63e46940b
Emma Zapanti <90106098+LilGhost44@users.noreply.github.com>
2025-05-04 13:09:45 +0300

    Add files via upload

---
+++ Application_Test.java
@@ -0,0 +1,6 @@
+public class Application_Test {
+    public static void main(String[] args) {
+        File test2 = new File("test2.txt","test2.txt","New file for testing\n <h2>I hope it works</h2>");
+        System.out.println(test2.getContent());
+    }
+}
